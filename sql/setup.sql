4aab97c81185ee191f30486f9a8020c8e78f96a9
GeoSauer <geoffrey.sauer89@gmail.com>
2023-02-22 19:33:28 -0700

    adds cors working

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS users, spells, lists CASCADE;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    -- //TODO change these back to unique for rls
     -- username TEXT UNIQUE,
     username TEXT,
     -- email TEXT UNIQUE,
@@ -12,6 +13,23 @@ CREATE TABLE users (
     char_name TEXT,
     char_class TEXT,
     caster_lvl INT
-    -- TODO figure out how to host avatars
+    -- //TODO figure out how to host avatars
     -- avatar_url TEXT,
 );
+
+-- CREATE TABLE spells (
+--         id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--         index TEXT,
+--         -- name TEXT,
+--         -- url TEXT
+-- );
+
+-- CREATE TABLE prepared (
+--         id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--         user_id BIGINT,
+--         FOREIGN KEY (user_id) REFERENCES users(id),
+--         spell_id BIGINT,
+--         FOREIGN KEY (spell_id) REFERENCES spells(id),
+--         preps INT,
+--         used INT
+-- );
